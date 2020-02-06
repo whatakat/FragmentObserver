94d1e933917fcc311920969c416bb88725985236
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-02-06 09:13:05 +0300

    create MainActivity

--- app/src/main/java/com/bankmtk/fragmentobserver/MainActivity.java
+++ app/src/main/java/com/bankmtk/fragmentobserver/MainActivity.java
@@ -1,14 +1,35 @@
 package com.bankmtk.fragmentobserver;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements PublishGetter{
+    private Publisher publisher = new Publisher();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Fragment1 fragment1 = new Fragment1();
+        Fragment2 fragment2 = new Fragment2();
+        MainFragment mainFragment = new MainFragment();
+
+        publisher.subscribe(fragment1);
+        publisher.subscribe(fragment2);
+
+        FragmentTransaction fragmentTransaction =
+                getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.fragment_main, mainFragment);
+        fragmentTransaction.add(R.id.fragment_1, fragment1);
+        fragmentTransaction.add(R.id.fragment_2, fragment2);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public Publisher getPublisher() {
+        return publisher;
     }
 }
