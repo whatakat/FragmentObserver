f4484d136b7e186677f0c3a26b306a7d64ff08b3
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-02-06 09:28:57 +0300

    add Fragment1

---
+++ app/src/main/java/com/bankmtk/fragmentobserver/Fragment1.java
@@ -0,0 +1,28 @@
+package com.bankmtk.fragmentobserver;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Fragment1 extends Fragment implements Observer {
+    private TextView textView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment1,container, false);
+        textView = view.findViewById(R.id.textView);
+        return view;
+    }
+
+    @Override
+    public void updateText(String text) {
+        textView.setText(text);
+    }
+}
